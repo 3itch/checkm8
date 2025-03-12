5ca54614d854a92e52e24c94f1dba887424610ef
3itch <torsten.oehlenschlager@tutanota.de>
2025-03-12 08:29:36 +0000

    checkmate:  added sinit ACM invokation and cleaned up a few functions

--- patch.c
+++ patch.c
@@ -18,6 +18,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <elf.h>
 #include "com.h"
 #include "cbfs_load.h"
 #include "coreboot_table.h"
@@ -25,7 +26,30 @@
 #define VERIFY_INTEGRITY_OFFSET     0x80af10
 #define CBFS_MASTER_HEADER_MAGIC  0x4F524243     //    orbc
 
+struct mle_param_block {
+  uint32_t  version;
+  uint32_t  mle_entry;
+  uint32_t  mle_start;
+  uint32_t  mle_end;
+  uint8_t   reserved[4];
+}
+
 void *CBFS_BASE_ADDR = NULL;
+
+void *sinit_load() {
+  size_t sinit_size;
+  void *sinit_data = cbfs_load("sinit.bin", &sinit_size);
+
+  if ( !sinit_data ) {
+    serial_print("checkmate:  couldn't load sinit.bin\n");
+    return NULL;
+  }
+
+  void *sinit_base = (void *)0x00000000;
+  memcpy(sinit_base, sinit_data, sinit_size);
+
+  return sinit_base;
+}
 
 void patch(const void* tboot_base) {
   void* verify_integrity_addr = (void*)( (uint32_t)tboot_base + VERIFY_INTEGRITY_OFFSET );
@@ -81,20 +105,38 @@ void *bruteforce_cbfs_base_addr(uintptr_t start, uintptr_t end, uint32_t step) {
   return NULL;
 }
 
+extern void call_sinit(void);
+void mpb_setup(void *tboot_entry, size_t tboot_size) {
+  struct mle_param_block mpb = {
+    .version = 6,
+    .mle_entry = (uint32_t)tboot_entry,
+    .mle_start = TBOOT_LOAD_ADDR,
+    .mle_end = TBOOT_LOAD_ADDR + tboot_size,
+    .reserved = {0},
+  };
+
+  void *mpb_base = (void *)0x1000;      // estimate,  adjust based on config or spec
+  memcpy(mpb_base, &mpb, sizeof(mpb));
+}
+
 //extern void* __cbfs_load(const char* name, size_t* size);
-void* tboot_load() {
-  size_t tboot_size;
-  void* tboot_base = cbfs_load("tboot-bin", &tboot_size);
-  if (!tboot_base) {
-    serial_print("checkmate:  failed to load tboot-bin\n");
-    return NULL;
+void *load_elf(void *data, size_t size) {
+  Elf32_Ehdr *ehdr = (Elf32_Ehdr *)data;
+  Elf32_Phdr *phdr = (Elf32_Phdr *)( data + ehdr->e_phoff );
+
+  int i;
+  for ( i = 0; i < ehdr->e_phnum; i++ ) {
+    if ( phdr[i].p_type == PT_LOAD ) {
+      if ( !*base_addr ) *base_addr = (void *)phdr[i].p_vaddr;
+      memcpy((void *)phdr[i].p_vaddr, data + phdr[i].p_offset, phdr[i].p_filesz);
+    }
   }
 
-  return tboot_base;
+  return (void *)ehdr->e_entry;
 }
 
 void shim_main() {
-  serial_print("checkmate:  shim running post-SINIT\n");
+  serial_print("checkmate:  preparing MLE -SINIT\n");
 
 //  uintptr_t search_start = 0xFF000000;     //  -16MB
 //  uintptr_t search_end   = 0xFFFFFFFF;     //    4GB
@@ -109,19 +151,34 @@ void shim_main() {
     return;
   }
 
-  void* tboot_base = tboot_load();
-  if (!tboot_base) {
-    serial_print("checkmate:      failed to load tboot\n");
+  size_t tboot_size;
+  void *tboot_data = cbfs_load("tboot-bin", &tboot_size);
+  if ( !tboot_data ) {
+    serial_print("checkmate:      failed to load tboot-bin\n");
+    return;
+  }
+
+  void *tboot_base;
+  void *tboot_entry = load_elf(tboot_data, tboot_size, &tboot_base);
+  if ( !tboot_entry ) {
+    serial_print("checkmate:      failed to parse tboot ELF\n");
     return;
   }
   
   serial_print("checkmate:  tboot loaded @ ");
-  serial_print_hex((uint32_t)tboot_base);
+  serial_print_hex((uint32_t)tboot_entry);
   serial_print("\n");
 
   patch(tboot_base);
 
-  serial_print("checkmate:  jumping to tboot entry\n");
-  void (*tboot_entry)(void) = (void (*)(void))tboot_base;
-  tboot_entry();
+  void *sinit_base = sinit_load();
+  if ( !sinit_base ) {
+    serial_print("checkmate:      failed to load sinit\n");
+    return;
+  }
+
+  mpb_setup(tboot_entry, tboot_size);
+
+  serial_print("checkmate:  invoking sinit\n");
+  call_sinit();
 }
